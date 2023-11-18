7b7b435baa32fb363dd40c0aac94418522010722
Dan Goje <gojedan98@gmail.com>
2023-11-19 00:09:18 +0200

    column sorting

--- spreadsheet/spreadsheet.py
+++ spreadsheet/spreadsheet.py
@@ -8,13 +8,13 @@ from sheet_primitives import CellKey, CellValue, Col, Row
 
 class Spreadsheet:
     def __init__(self):
-        self._cells: dict[Row, dict[Col, CellValue]] = {}
+        self._cells: list[dict[Col, CellValue]] = []
 
     def set_cell(self, cell_key: CellKey, cell_value: str) -> None:
         row, col = self._parse_key(cell_key)
 
-        if row not in self._cells:
-            self._cells[row] = {}
+        if row >= len(self._cells):
+            self._extend_rows(2 * row + 1)
 
 
         stripped_val = cell_value.strip()
@@ -29,6 +29,22 @@ class Spreadsheet:
         else:
             self._cells[row][col] = cell_value
 
+    def get_cell(self, cell_key: Union[CellKey]) -> CellValue:
+        return self._get_cell(cell_key)
+
+    def sort_rows_by_column(self, col: Col, reverse = False) -> None:
+        to_sort = sorted(
+            filter(lambda row: col in row, self._cells),
+            key=lambda row: row[col],
+            reverse=reverse
+        )
+        no_val = filter(lambda row: col not in row, self._cells)
+        self._cells = to_sort + list(no_val)
+
+
+    def _extend_rows(self, num_rows: int) -> None:
+        for _ in range(num_rows - len(self._cells)):
+            self._cells.append({})
     
     def _set_int(self, row: Row, col: Col, val: str) -> bool:
         try:
@@ -43,9 +59,6 @@ class Spreadsheet:
             return True
         except ValueError:
             return False
-
-    def get_cell(self, cell_key: Union[CellKey]) -> CellValue:
-        return self._get_cell(cell_key)
 
     def _get_cell(self, cell_key: CellKey) -> CellValue:
         row, col = self._parse_key(cell_key)
@@ -69,16 +82,17 @@ class Spreadsheet:
             raise KeyError("Malformed cell key. Expected first half to be a character between A and Z.")
 
         try:
-            row = int(cell_key[1:])
+            row = int(cell_key[1:]) - 1
         except ValueError:
             raise KeyError("Malformed cell key. Expected second half to be integer row identifier")
+        if row < 0:
+            raise KeyError("Malformed cell key. Expected row identifier to be greater than 0.")
 
         return row, col
 
 
 
-# TODO: Column sorting
-# TODO: COlumn filtering
+# TODO: Column filtering
 # TODO: Handle ranges and functions (start with SUM over a range)
 # TODO: Implement performance profiler
 if __name__ == "__main__":
@@ -106,11 +120,11 @@ if __name__ == "__main__":
     assert s.get_cell("A12") == 10
 
     s.set_cell("A14", "=-A1")
-    assert s.get_cell("A14") == -cast(int, s.get_cell("A1"))
+    assert s.get_cell("A14") == -int(s.get_cell("A1"))
     s.set_cell("A13", "=-A12")
-    assert s.get_cell("A12") == -cast(int, s.get_cell("A13"))
+    assert s.get_cell("A12") == -int(s.get_cell("A13"))
     s.set_cell("A13", "=-(A12+A12)")
-    assert 2 * cast(int, s.get_cell("A12")) == -cast(int, s.get_cell("A13"))
+    assert 2 * int(s.get_cell("A12")) == -int(s.get_cell("A13"))
     s.set_cell("A13", "=-A12+A12")
     assert s.get_cell("A13") == 0
     s.set_cell("A13", "=-(A12)+A12")
@@ -168,3 +182,19 @@ if __name__ == "__main__":
     assert s.get_cell("A3") == 3.4 + 2
     s.set_cell("A3", "=A1+2.3")
     assert s.get_cell("A3") == 3.4 + 2.3
+
+    new_s = Spreadsheet()
+    new_s.set_cell("A1", "3")
+    new_s.set_cell("B1", "a")
+    new_s.set_cell("A2", "1")
+    new_s.set_cell("B2", "b")
+
+    new_s.sort_rows_by_column("A")
+    assert new_s.get_cell("A1") == 1
+    assert new_s.get_cell("B1") == "b"
+    new_s.sort_rows_by_column("B")
+    assert new_s.get_cell("A1") == 3
+    assert new_s.get_cell("B1") == "a"
+    new_s.sort_rows_by_column("B", reverse=True)
+    assert new_s.get_cell("A1") == 1
+    assert new_s.get_cell("B1") == "b"
